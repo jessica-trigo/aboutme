d1a02aedafb3e85e7365b4c39a9d1082dca33627
Jéssica Trigo <jessica.trigo05@icloud.com>
2025-07-14 23:14:44 +0200

    "ready to deploy"

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
 import path from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [tailwindcss(), svelte() ],
-    resolve: {
+  base: "/aboutme/",
+  plugins: [tailwindcss(), svelte()],
+  resolve: {
     alias: {
       $lib: path.resolve("./src/lib"),
     },
